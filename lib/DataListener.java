e8d49356347a03fcf42a2b486aa81692c6df942e
Karim Alibhai <karim@alibhai.co>
2016-09-19 19:53:50 -0400

    add NetworkStream abstract class

---
+++ lib/DataListener.java
@@ -0,0 +1,10 @@
+/**
+ * lib/DataListener.java - lab2
+ * A simple class for creating data events.
+ */
+
+package lib;
+
+interface DataListener {
+    public abstract void eventHandler(String data);
+}
---
+++ lib/NetworkStream.java
@@ -0,0 +1,44 @@
+/**
+ * lib/NetworkStream.java - lab2
+ * An interface for the TCPStream and UDPStream classes.
+ */
+
+package lib;
+
+import java.util.ArrayList;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+abstract class NetworkStream {
+    protected Socket socket = null;
+    protected ServerSocket server = null;
+
+    /**
+     * Send data across the socket.
+     * @param data the data to send over in string form
+     */
+    public abstract void send(String data);
+
+    private ArrayList<DataListener> onDataListeners = new ArrayList<DataListener>();
+
+    /**
+     * Add an event listener for when data is received
+     * from the other side.
+     * @param listener an instance of DataListener to act as an event handler
+     */
+    public void receive(DataListener listener) {
+        this.onDataListeners.add(listener);
+    }
+
+    /**
+     * Dispatches the data event for all listeners with
+     * given data. Not a public method.
+     *
+     * @param data the data to send to event handlers
+     */
+    protected void dispatchDataListener(String data) {
+        for (DataListener listener : this.onDataListeners) {
+            listener.eventHandler(data);
+        }
+    }
+}
