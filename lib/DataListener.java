6d29715cd68b66d9bcc39c35f77a4447c277a17a
Karim Alibhai <karim@alibhai.co>
2016-09-19 19:54:59 -0400

    change DataListener to abstract class

--- lib/DataListener.java
+++ lib/DataListener.java
@@ -5,6 +5,10 @@
 
 package lib;
 
-interface DataListener {
+abstract class DataListener {
+    /**
+     * The method that will be called when the event is dispatched.
+     * @param data the data that was received
+     */
     public abstract void eventHandler(String data);
 }
