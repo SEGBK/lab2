4abb7389394f9228c325e4fed1f51dfd8bfa4211
Brennan McDonald <brennan.mcdonald.mail@gmail.com>
2016-09-22 11:30:00 -0400

    Changed Runner.java to not assert an always false test

--- test/Runner.java
+++ test/Runner.java
@@ -18,7 +18,7 @@ public final class Runner {
             new Test("Test basic test runner") {
                 public void test() {
                     Test.equal(10, 10, "10 should equal 10");
-                    Test.equal(10, 15, "10 should not equal 15");
+                    // Test.equal(10, 15, "10 should not equal 15");
                 }
             },
             //new DataListenerTest(),
