8ec43a7defbd18231a0da9e9687343710619e98f
Karim Alibhai <karim@alibhai.co>
2016-09-21 23:42:12 -0400

    add basic game starter

---
+++ Game.java
@@ -0,0 +1,12 @@
+/**
+ * Game.java - lab2
+ * Starts up the initial view frame.
+ */
+
+import views.*;
+
+class Game {
+    public static void main(String[] args) {
+        new StartView();
+    }
+}
