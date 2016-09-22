d76d26a245224dc5e694e03021a7c752eaab07b3
Karim Alibhai <karim@alibhai.co>
2016-09-22 00:30:03 -0400

    update test runner

--- test/Runner.java
+++ test/Runner.java
@@ -6,17 +6,40 @@
 package test;
 
 public final class Runner {
-    public static <T> void equal(T expected, T given, String message) {
-        System.out.format(
-            "\u001b[%dm %s %s%s\u001b[39m\n",
-            expected == given ? 32 : 31,
-            expected == given ? "\u2713" : "x",
-            message,
-            expected != given ? " (expected " + expected + ", got " + given + ")" : ""
-        );
-    }
-
     public static void main(String[] args) {
-        Runner.equal(10, 15, "things should have happened");
+        int attempted = 0, passed = 0;
+
+        for (Test test : new Test[] {
+            /**
+             * List of tests to run.
+             * All the objects below should implement
+             * the interface Test.
+             */
+            new Test("Test basic test runner") {
+                public void test() {
+                    Test.equal(10, 10, "10 should equal 10");
+                    Test.equal(10, 15, "10 should not equal 15");
+                }
+            },
+            //new DataListenerTest(),
+            //new TCPStreamClientTest(),
+            //new TCPStreamServerTest(),
+            //new UDPStreamClientTest(),
+            //new UDPStreamServerTest()
+        }) {
+            attempted += 1;
+            if (test.run()) passed += 1;
+        }
+
+        // if any tests failed, report failure to the shell
+        // and the user
+        System.out.format(
+            "\n\u001b[%dm%d/%d Tests passed.\u001b[39m\n\n",
+            passed < attempted ? 31 : 32,
+            passed,
+            attempted
+        );
+
+        System.exit(passed < attempted ? -1 : 0);
     }
 }
