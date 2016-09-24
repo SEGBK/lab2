c3678b9a9ab5604217bc4c172471881c5925e170
Karim Alibhai <karim@alibhai.co>
2016-09-24 18:56:39 -0400

    implement echo server with tests

--- test/TCPStreamClientTest.java
+++ test/TCPStreamClientTest.java
@@ -6,6 +6,7 @@
 package test;
 
 import lib.TCPStream;
+import lib.DataListener;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.BufferedReader;
@@ -22,6 +23,18 @@ class TCPStreamClientTest extends Test {
         // create a new instance of TCPStream as client
         final TCPStream client = new TCPStream("localhost", 8080);
 
+        // array of inputs that should also be outputs
+        final String[] inputs = "1,2,3,4,5,6,7,8,9".split(",");
+
+        // create a wrapper for the end
+        final FBool expectDone = new FBool();
+        final Runnable done = new Runnable() {
+            public void run() {
+                Test.equal(expectDone.state(), true, "should not end prematurely");
+                end.run();
+            }
+        };
+
         // at this time, the socket should be closed
         Test.equal(client.isOpen(), false, "socket should be closed initially");
 
@@ -32,14 +45,71 @@ class TCPStreamClientTest extends Test {
                 Test.equal(testRun, false, "should only call onConnect once");
                 Test.equal(client.isOpen(), true, "socket should be open during onConnect");
                 testRun = true;
-                end.run();
+
+                // write all inputs to pipe
+                for (String data : inputs) {
+                    System.out.format("send :: %s\n", data);
+                    client.send(data);
+                }
+            }
+        });
+
+        // bind the end wrapper to the disconnect event
+        client.onDisconnect(done);
+
+        // bind to the error event
+        client.onError(new DataListener() {
+            public void eventHandler(String error) {
+                Test.equal(error, "null", "should never error out");
+            }
+        });
+
+        // bind to the data event
+        client.receive(new DataListener() {
+            private int i = 0;
+            public void eventHandler(String data) {
+                String[] parsed = data.split(":");
+                
+                Test.equal(parsed.length, 2, "should receive two-part data");
+                Test.equal(parsed[0], "server", "should receive server headers as data");
+                
+                try {
+                    Integer.parseInt(parsed[1], 10);
+                    Test.equal(true, true, "should parse second part as integer");
+                } catch (NumberFormatException ex) {
+                    Test.equal(true, false, "should not fail to parse integer");
+                }
+
+                if ((++ i) == inputs.length) {
+                    expectDone.set(true);
+                    client.close();
+                }
             }
         });
 
         // now we can accept a connection
-        Socket server = new ServerSocket(8080).accept();
+        final Socket server = new ServerSocket(8080).accept();
 
-        // now, the socket should be open
-        Test.equal(client.isOpen(), true, "socket should be open after accepting connection");
+        // setup server as an echo server
+        new Thread() {
+            private BufferedReader reader = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            private BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
+
+            public void run() {
+                // loop and echo the data back, if things
+                // go wrong, just ignore
+                try {
+                    while (true) {
+                        while (!this.reader.ready());
+
+                        String rl = this.reader.readLine();
+                        System.out.format("server echoes => %s\n", rl);
+                        this.writer.write("server:" + rl);
+                        this.writer.newLine();
+                        this.writer.flush();
+                    }
+                } catch (Throwable th) {System.out.format("%s\n",th.getMessage());}
+            }
+        }.start();
     }
 }
