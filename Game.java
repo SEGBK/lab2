afdf663366023a2bb00b8e2bec4398dab5d91854
Brennan McDonald <brennan.mcdonald.mail@gmail.com>
2016-09-26 23:10:40 -0400

    Game has proper methods, Minor changed required to integrate into network streams

--- Game.java
+++ Game.java
@@ -7,6 +7,21 @@ import lib.*;
 
 class Game {
     public static void main(String[] args) {
-        // ...
+		Board b = new Board();
+		System.out.println(b.toString());   
+		b.play(0,0);
+		System.out.println(b.toString());        
+		b.play(0,1);
+		System.out.println(b.toString());        
+		b.play(0,2);
+		System.out.println(b.toString());        
+		b.play(1,1);
+		System.out.println(b.toString());        
+		b.play(1,0);
+		System.out.println(b.toString());        
+		b.play(2,2);
+		System.out.println(b.toString());        
+		b.play(2,0);
+		System.out.println(b.toString());        
     }
 }
