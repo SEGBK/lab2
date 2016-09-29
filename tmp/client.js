8328107f643786c9d4f0098baa4e6d286c36b82c
Karim Alibhai <karim@alibhai.co>
2016-09-29 00:12:33 -0400

    remove temporary files

--- tmp/client.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-var socket = require('net').connect(8080)
-socket.on('data', console.log.bind(console, 'data: %s'))
-setInterval(() => {let d=Math.random();console.log('send :: %s',d),socket.write(d+'\n')}, 1000)
--- tmp/server.js
+++
@@ -1,4 +0,0 @@
-require('net').createServer((sock) => {
-  sock.on('data', console.log.bind(console,'data: %s'))
-  setInterval(()=>sock.write(Math.random()+'\n'),1000)
-}).listen(8080)
